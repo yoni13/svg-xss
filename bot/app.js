9b2f9b798e36f54376d11f5faa77740fcb2105cc
yoni980807@gmail.com <yoni980807@gmail.com>
2023-07-15 16:09:23 +0800

    please stable

--- bot/app.js
+++ bot/app.js
@@ -1,37 +1,26 @@
 const net = require('net')
+
 const visit = require('./bot')
+
 const dns = require('dns/promises')
 
 const PORT = process.env.PORT
 const REPORT_HOST = process.env.REPORT_HOST
 
-dns.lookup(REPORT_HOST).then(({ address }) => {
-	const server = net.createServer(async socket => {
-		if (!socket.remoteAddress.endsWith(address)) {
-			// remoteAddress may be ipv4 mapped ipv6 address
-			socket.end('Bad reporting host')
-			return
-		}
-		socket.on('data', async data => {
-			try {
-				const url = data.toString().trim()
-				socket.end('URL received')
-				socket.destroy()
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const url = require('url');
+const querystring = require('querystring');
 
-				if (!url.match(/^https?:\/\//)) {
-					console.log(`[-] Invalid URL: ${url}`)
-					return;
-				}
+app.get("/", function (req, res) {
+  url = req.query.report
+  visit(URL);
+  res.send(url);
+});
 
-				console.log(`[+] Received: ${url}`)
-				await visit(url)
-				console.log(`[+] Visited: ${url}`)
-			} catch (e) {
-				console.log(e)
-			}
-		})
-	})
-	server.listen(PORT, () => {
-		console.log('Bot socket server listening on port', PORT)
-	})
-})
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
+
